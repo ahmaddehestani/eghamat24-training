25f0a9546c9bf75700e5529d4bf0a08ca30a5d19
AhmadDehestani <s.ahmad.dehestani@gmail.com>
2022-08-13 11:40:54 +0430

    feat:gulp sass to css

--- gulp test/gulpfile.js
+++ gulp test/gulpfile.js
@@ -1,14 +1,20 @@
 
 
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
 const cleanCSS = require('gulp-clean-css');
+const sass = require('gulp-sass')(require('sass'));
 
  
 gulp.task('script', function () {
   return gulp.src('./script/*js')
   .pipe(uglify())
   .pipe(gulp.dest('./dist'))
+});
+gulp.task('sass',function buildStyles() {
+  return gulp.src('./style/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
 });
 
 
