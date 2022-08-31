50d76c4990b880e0f978e4cf4c30c1f1aacd7dd2
AhmadDehestani <s.ahmad.dehestani@gmail.com>
2022-08-31 22:51:33 +0430

    feat:index php for get url from client

---
+++ crawler/index.php
@@ -0,0 +1,32 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./main.css">
+    <title>crawler</title>
+
+
+
+</head>
+
+<body>
+    <form method="post" action="index.php">
+        <img src="./SEO.png" alt="Girl in a jacket">
+        <label>please insert url like example ( http://eghamat24.com/ )</label>
+        <input type="text" name="url" class="title" value="url" />
+        <button> analyze website</button>
+
+
+
+
+    </form>
+
+</body>
+
+</html>
