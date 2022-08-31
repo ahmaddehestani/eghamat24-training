626a5fb0add20aac35fb5c020692025f5094076b
AhmadDehestani <s.ahmad.dehestani@gmail.com>
2022-08-31 20:06:16 +0430

    feat:get all link in page crawling

---
+++ crawler/index.php
@@ -0,0 +1,30 @@
+<?php
+
+
+require('./vendor/autoload.php');
+
+
+
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\CssSelector\CssSelectorConverter;
+use GuzzleHttp\Client;
+
+
+
+$url = "http://eghamat24.com/";
+$links = array();
+$images = array();
+$videos = array();
+$metas = array();
+
+
+$response = file_get_contents($url);
+
+$crawler = new Crawler ($response);
+$links = $crawler->filter('a')->each(function (Crawler $node, $i) {
+    $temporary = $node->attr('href');
+    $link = $temporary;
+    return $link;
+});
+
+
